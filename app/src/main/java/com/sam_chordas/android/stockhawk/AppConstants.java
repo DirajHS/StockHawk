0c9df40f5b287cc58d9ed3b6800eb46304197210
Diraj H S <dirajhsdj@gmail.com>
2016-10-04 11:53:27 +0530

    Fixed App crash when searched for non existing stock

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/AppConstants.java
@@ -0,0 +1,17 @@
+package com.sam_chordas.android.stockhawk;
+
+/**
+ * Created by Diraj H S on 10/4/16.
+ * Copyright (c) 2016. All rights reserved.
+ */
+
+public class AppConstants {
+
+    public static final String JSON_QUERY = "query";
+    public static final String JSON_COUNT = "count";
+    public static final String JSON_RESULTS = "results";
+    public static final String JSON_QUOTE = "quote";
+    public static final String JSON_CHANGE = "Change";
+    public static final String JSON_CHANGE_PERCENTAGE = "ChangeinPercent";
+    public static final String JSON_BID = "Bid";
+}
